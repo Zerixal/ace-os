0956c9cbd1924797557540ab6c202c0f56a6e2a1
aceday <markcediebuday@gmail.com>
2026-05-21 16:31:13 +0800

    ace to fedora for iso

--- build-scripts/modules/base/system.sh
+++ build-scripts/modules/base/system.sh
@@ -8,7 +8,7 @@ DATE=$(date +%Y%m%d)
 echo "ace" | tee "/etc/hostname"
 sed -i -f - /usr/lib/os-release <<EOF
 s|^NAME=.*|NAME=\"Ace\"|
-s|^ID=.*|ID=\"ace\"|
+s|^ID=.*|ID=\"fedora\"|
 s|^VERSION=.*|VERSION=\"${RELEASE}.${DATE}\"|
 s|^PRETTY_NAME=.*|PRETTY_NAME=\"Ace ${RELEASE}.${DATE}\"|
 s|^LOGO=.*|LOGO=\"cachyos\"|
