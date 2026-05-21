36554d0010e2c9804717457b4dfaa986a72c3e0f
aceday <markcediebuday@gmail.com>
2026-05-21 14:54:33 +0800

    plasmalogin

--- build-scripts/modules/de/kde/packages.sh
+++ build-scripts/modules/de/kde/packages.sh
@@ -14,7 +14,9 @@ cat > /usr/lib/systemd/system-preset/80-ace-kde.preset <<'EOF'
 enable sddm.service
 EOF
 
-if [ -f /usr/lib/systemd/system/sddm.service ] || [ -f /etc/systemd/system/sddm.service ]; then
+if [ -L /etc/systemd/system/display-manager.service ]; then
+  echo "display-manager.service already set; skipping sddm enable"
+elif [ -f /usr/lib/systemd/system/sddm.service ] || [ -f /etc/systemd/system/sddm.service ]; then
   systemctl enable sddm.service
 else
   echo "sddm.service not found; skipping enable"
